f39cc7432f265b3c542fac848190a59f012db854
MC <G00419267@atu.ie>
2023-10-24 13:37:49 +0100

    Started student construction

---
+++ src/main/java/atu/ie/main.java
@@ -0,0 +1,6 @@
+package atu.ie;
+
+public class main {
+
+
+}
---
+++ src/main/java/atu/ie/student.java
@@ -0,0 +1,17 @@
+package atu.ie;
+
+public class student {
+    String sname;
+    String semail;
+    String scourse;
+
+    public Student(String name,String email,String course){
+        this.sname = name;
+        this.semail = email;
+        this.scourse = course;
+    }
+
+    public static void main(String[] args){
+
+    }
+}
